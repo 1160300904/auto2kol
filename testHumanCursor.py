8087662e250f552307db6c7e893d7e01389fd461
1160300904 <1304054655@qq.com>
2024-04-09 15:48:22 +0800

    commit

--- testHumanCursor.py
+++ testHumanCursor.py
@@ -3,4 +3,4 @@ from humancursor import  SystemCursor
 cursor=SystemCursor()
 
 #cursor.move_to([200,100])
-cursor.click_on(point=[200,300],click_duration=0.01)
+cursor.click_on(point=[200,300],click_duration=0.001)
